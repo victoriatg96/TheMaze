a9d153c5bacf7b24127036a4c202b164b3ee31a8
Victoria <victoriataing05@gmail.com>
2017-12-18 18:04:22 +0100

    test

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    public bool canMove;
+    public float moveSpeed = 5;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get user's current movement input
+        Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"));
+
+      
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("inputX", movementVector.x);
+            anim.SetFloat("inputY", movementVector.y);
+
+            rbody.MovePosition(rbody.position + movementVector * moveSpeed * Time.deltaTime);
+      
+    }
+}
