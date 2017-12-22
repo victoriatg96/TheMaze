be8a023f1baa586e7ecba387b0752bf805054d87
Ludo <ludovic.ardin@devinci.fr>
2017-12-22 19:24:14 +0100

    Première version

---
+++ Assets/Script/DamageManagement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManagement : MonoBehaviour
+{
+	//public float HP;
+	public int HP = 5;
+	public int cp = 1;
+	//bool HP = 100 = true;
+	public GameObject vie1;
+	public GameObject vie2;
+	public GameObject vie3;
+	public GameObject vie4;
+	public GameObject vie5;
+	public GameObject cp1;
+	public GameObject j1g;
+	public GameObject j1g2;
+	// Use this for initialization
+	void Start () {
+		HP = 5;
+		//Camp1.SetActive (false);
+
+	}
+	
+	// Update is	 called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("Bonus")) {
+			HP++;
+			if (HP > 5)
+				HP = 5;
+
+			switch (HP) {
+			case 5:
+				vie5.SetActive (true);
+				break;
+
+			case 4:
+				vie4.SetActive (true);
+				break;
+
+			case 3:
+				vie3.SetActive (true);
+				break;
+
+			case 2:
+				vie2.SetActive (true);
+				break;
+
+			case 1:
+				vie1.SetActive (true);
+				break;
+			}
+
+			Destroy (other.gameObject);
+		} else if (other.gameObject.CompareTag ("Player")) {
+
+		} else if (other.gameObject.CompareTag ("Wall")) {
+			HP--;
+
+			switch (HP) {
+			case 4:
+				vie5.SetActive (false);
+				break;
+
+			case 3:
+				vie4.SetActive (false);
+				break;
+
+			case 2:
+				vie3.SetActive (false);
+				break;
+
+			case 1:
+				vie2.SetActive (false);
+				break;
+
+			case 0:
+				vie1.SetActive (false);
+				j1g2.SetActive (true);
+				break;
+			}
+		} else if (other.gameObject.CompareTag ("Camp1")) {
+			cp--;
+
+
+			cp1.SetActive (false);
+
+			if (cp < 1) {
+				
+
+					j1g.SetActive (true);
+
+
+			}
+		}
+
+		else if (other.gameObject.CompareTag ("Camp2")) {
+		}
+
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int id;
+	public float Speed = 10f;
+	public float jump = 20f;
+	//public float Gravite = 0;
+	private Vector2 DirectionDeplacement = Vector2.zero;
+	//private Vector3 DirectionDeplacement = Vector3.zero;
+	//private CharacterController Player;
+
+	public float horizontalSpeed = 2.0F;
+	public float verticalSpeed = 2.0F;
+
+	void Start () {
+		//Player = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DirectionDeplacement = Vector2.zero;
+		DirectionDeplacement.x = Input.GetAxisRaw ("Horizontal j" + id);
+		DirectionDeplacement.y = Input.GetAxisRaw ("Vertical j" + id);
+
+		//DirectionDeplacement = transform.TransformDirection(DirectionDeplacement);
+		//transform.Translate (new Vector3(DirectionDeplacement.x  * Time.deltaTime * Speed, DirectionDeplacement.y * Time.deltaTime * Speed, 0.0f));
+
+		//Player.Move (DirectionDeplacement * Speed * Time.deltaTime);
+
+
+		/*
+		float h = horizontalSpeed * Input.GetAxis ("Mouse X");
+		float v = verticalSpeed * Input.GetAxis ("Mouse Y");
+		transform.Rotate (v, h, 0);
+*/
+		/*
+		if (!Player.isGrounded) {
+			DirectionDeplacement.y = Gravite * Time.deltaTime * Speed;
+		}
+		*/
+		/*
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			DirectionDeplacement.y = jump * Speed * Gravite;
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			DirectionDeplacement.y -= jump * Speed * Gravite;
+		}
+		*/
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + DirectionDeplacement * Speed * Time.fixedDeltaTime);
+	}
+}
